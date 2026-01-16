da09a17b7a9e111e28d20e56cf8e4c2cdb959dac
Alice Antonello <aliceantonello9@gmail.com>
2026-01-16 12:40:22 +0100

    start of the vignette drafting

--- R/data.R
+++ R/data.R
@@ -46,3 +46,27 @@
 #'
 "exampleFit"
 
+#' Input data for patient UPN06
+#'
+#' @docType data
+#'
+#' @usage data(UPN06)
+#'
+#' @format A list of dataframes with the input of TOSCA
+#'
+#' @keywords datasets
+#'
+"UPN06"
+
+#' Input data for patient D9MRCY
+#'
+#' @docType data
+#'
+#' @usage data(D9MRCY)
+#'
+#' @format A list of dataframes with the input of TOSCA
+#'
+#' @keywords datasets
+#'
+"D9MRCY"
+
